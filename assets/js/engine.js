6a7ab6d5cba0e9b0ea229cff4188ad933ed096b0
Hasin Hayder <countdraculla@gmail.com>
2018-06-12 19:46:47 +0600

    cleanup

---
+++ assets/js/engine.js
@@ -0,0 +1,78 @@
+;(function ($) {
+    $(document).ready(function () {
+        $("li").on("click", function () {
+            var c = $(this).attr('class');
+            $(".fixtures-body tr").hide();
+            $(".fixtures-body tr." + c).show();
+        });
+
+        $(".filter").on("click", function () {
+            var filter = $(this).data("filter");
+            $(".fixtures-body tr").hide();
+            $(".fixtures-body tr." + filter).show();
+        });
+
+        //console.log(countries);
+
+        for (var i in fixtures) {
+            var class1 = countries[fixtures[i].p1] ? countries[fixtures[i].p1].code : "w";
+            var class2 = countries[fixtures[i].p2] ? countries[fixtures[i].p2].code : "w";
+
+            var flag1 = countries[fixtures[i].p1] ? '<img class="mr-1" src="http://www.countryflags.io/' + countries[fixtures[i].p1].code + '/flat/32.png" alt="' + fixtures[i].p1 + '">' : "";
+            var flag2 = countries[fixtures[i].p2] ? '<img class="mr-1" src="http://www.countryflags.io/' + countries[fixtures[i].p2].code + '/flat/32.png" alt="' + fixtures[i].p2 + '">' : "";
+
+            var class3 = "";
+            if (i >= 49 && i <= 56) class3 = "k";
+            if (i >= 57 && i <= 60) class3 = "qf";
+            if (i >= 61 && i <= 62) class3 = "sf";
+            if (i == 63) class3 = "f";
+            var td0 = $("<td/>").html(i).attr("width", "5%");
+            var td1 = $("<td/>").html(flag1 + toTitleCase(fixtures[i].p1)).attr("width", "25%");
+            var td2 = $("<td/>").html(flag2 + toTitleCase(fixtures[i].p2)).attr("width", "25%");
+            var td3 = $("<td/>").html(fixtures[i].date).attr("width", "25%").addClass("date").data("date", fixtures[i].date);
+            var td4 = $("<td/>").html(fixtures[i].time).attr("width", "20%").addClass("time").data("t24", fixtures[i].time24);
+            $("<tr/>").attr("id", "g" + fixtures[i].id).append([td0, td1, td2, td3, td4]).data({
+                "p1": fixtures[i].p1,
+                "p2": fixtures[i].p2,
+                "t24": fixtures[i].time24,
+                "t": fixtures[i].time,
+                "d": fixtures[i].date
+            }).addClass(['all', class1, class2, class3]).appendTo($(".fixtures-body"));
+        }
+
+        $("#timezone").on('change', function () {
+            var tz = parseFloat($(this).val());
+            var tzMinutes = (tz - parseInt(tz)) * 60;
+            tzMinutes = tzMinutes ? tzMinutes : "00";
+            var tzHours = parseInt(tz);
+            if (tz > 0) {
+                $(".fixtures-time-heading").html("Time (UTC/GMT + " + tzHours + ":" + tzMinutes + ")");
+            } else {
+                $(".fixtures-time-heading").html("Time (UTC/GMT - " + Math.abs(tzHours) + ":" + tzMinutes + ")");
+            }
+            convertTimeZone(tz);
+        });
+
+        convertTimeZone(); //Let's Set time to Bangladesh Time
+
+    });
+})(jQuery);
+
+function convertTimeZone(tz = "6") {
+    $(".fixtures-body tr").each(function () {
+        var date = $(this).find(".date").data("date");
+        var time = $(this).find(".time").data("t24");
+        var dt = new Date(date + " " + time);
+        dt.setTime(dt.getTime() + parseFloat(tz) * 3600 * 1000);
+        var newDate = dt.toLocaleString('en-US', {month: "long"}) + " " + dt.getDate() + ", " + dt.getFullYear();
+        var newTime = dt.toLocaleString('en-US', {hour: 'numeric', hour12: true, minute: 'numeric'});
+        $(this).find(".date").html(newDate);
+        $(this).find(".time").html(newTime);
+    });
+}
+
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
